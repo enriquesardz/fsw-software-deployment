58a005a8ebf1b1ba66bac68236b2493a93d45a77
ensardz <enriquesaalas@gmail.com>
2017-10-08 21:28:36 -0500

    First commit Hello World

---
+++ HW1/index.js
@@ -0,0 +1,11 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    
+    res.end('Hello World\n')
+
+}).listen(8080);
+
+console.log('Server running on port 8080.');
